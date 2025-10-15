667e41ace036e73e32a808274ae234fb72f8c8cd
Dandelion <86636793+Dan5N@users.noreply.github.com>
2025-10-15 15:56:51 +0800

    Fix import statements in dynamic_modules_utils.py

--- diffusers/utils/dynamic_modules_utils.py
+++ diffusers/utils/dynamic_modules_utils.py
@@ -25,7 +25,7 @@ from pathlib import Path
 from typing import Dict, Optional, Union
 from urllib import request
 
-from huggingface hub import HfFolder.hf hub download, model info
+from huggingface_hub import HfFolder, hf_hub_download, model_info
 from packaging import version
 
 from .. import __version__
